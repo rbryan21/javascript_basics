248c4ab5648e0d70b639a6a81bf7f338b2889d61
David Guevara <ideabox.by.dave@gmail.com>
2016-10-13 18:43:02 -0400

    Created assignment #5 for David Guevara.   Javascript basics

---
+++ 01_Hello_World/js/borders.js
@@ -0,0 +1,16 @@
+﻿// borders3.js
+// An example of a functon called by color,font,style selectors 
+// it will determine the color, font and style of the table's contents
+
+function formatColor(value) {            
+    document.getElementById("tbl").style.backgroundColor = "#" + value;    
+}
+
+
+function formatSize(value) {
+    document.getElementById("tbl").style.fontSize = value + "px";
+}
+
+function formatStyle(value) {
+    document.getElementById("tbl").style.fontStyle = value;
+}
