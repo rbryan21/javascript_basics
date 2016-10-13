5de9a951a8a7ab6378756f82f93b2f89557887ee
FatThompson <matthewbiggiethompson@gmail.com>
2016-10-13 19:19:00 -0400

    Added a for loop. I believe this covers everything.

--- 01_Hello_World/index.js
+++ 01_Hello_World/index.js
@@ -1,5 +1,22 @@
 // JavaScript function definition here
+/**
+* The main function
+*/
 function display(){
   console.log("button clicked");
-  $(".mainDIV").append("<p>No</p>");
+  var temp = forloop();
+  $(".mainDIV").append("<p>No... now here is your forLoop, Ya JERK. "+ temp +"</p>");
+}
+/**
+* The loop as requested
+*/
+function forloop(){
+  var temp="";
+  var array = [0];
+  for(i=0;i<9;i++){
+    //push a random value into the array
+    array.push(Math.floor(Math.random()*10));
+  }
+  console.log(array);
+  return array;
 }
