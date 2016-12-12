9fc19f6c812aac807fcb96464a8d30bc773ab2a6
Cengiz Gunay <cengique@users.sf.net>
2016-12-12 01:08:20 -0500

    Revert "Merge pull request #28 from ltoornman/master"
    This reverts commit 593ce50a193e8a2fe35e2134d9aa18a481ac860d, reversing
    changes made to fd28a87dcd9be8fc3de0966182f765a3e7187cfe.

--- 01_Hello_World/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$hostname = "localhost";
-	$username = "root";
-	$password = "admin";
-	$dbname = "mytestdatabase";
-
-	$dbc = mysqli_connect($hostname, $username, $password, $dbname) OR die ("could not connect to database, ERROR:" .mysqli_connect_error());
-
-?>
